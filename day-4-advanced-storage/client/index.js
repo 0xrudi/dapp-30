a988d78120420f4fe94784eb250629e4a2d251bd
rudnickandrew <andrewstevenrudnick@gmail.com>
2023-02-28 10:23:07 -0800

    fixing bugs in the front end

--- day-4-advanced-storage/client/index.js
+++ day-4-advanced-storage/client/index.js
@@ -7,24 +7,25 @@ let advancedStorage;
 // this function will create a web3 instance
 const initWeb3 = () => {
     return new Promise((resolve, reject) => {
-        // case 1: old metamask is present - the old metamask used an injected web3 object
-        if(typeof window.web3 !== 'undefined') {
-            return resolve(
-                new Web3(window.web3.currentProvider)
-            );
-        }
-        // case 2: new metamask is present
-        if(typeof window.web3 !== 'undefined') {
-            window.etherum.enable()
+        // case 1: new metamask is present
+        if(typeof window.ethereum !== 'undefined') {
+            const web3 = new Web3(window.ethereum);
+            window.ethereum.enable()
                 .then(() => {
                     resolve(
-                        new Web3(window.web3.ethereum)
+                        new Web3(window.ethereum)
                     );
                 })
                 .catch(e => {
                     reject(e)
                 });
             return;
+        }
+        // case 2: old metamask is present - the old metamask used an injected web3 object
+        if(typeof window.web3 !== 'undefined') {
+            return resolve(
+                new Web3(window.web3.currentProvider)
+            );
         }
         // case 3: no metamask is present, just connect to Ganache
         resolve(new Web3('http://localhost:9545'));
@@ -43,34 +44,35 @@ const initContract = () => {
 // this will be used to initalize the application 
 const initApp = () => {
     const $addData = document.getElementById('addData');
-    const $data = document.getAnimations('data');
+    const $data = document.getElementById('data');
     let accounts = []
 
     web3.eth.getAccounts()
-        .then(_accounts => {
-            accounts = accounts;
-            return advancedStorage.methods
-                .getAll()
-                .call();
-        })
-        .then(result => {
-            $data.innerHTML = result.join(', ');
-        });
-    
-        $addData.addEventListener('submit', e=> {
-            e.preventDefault();
-            const data = e.target.elements[0].value;
-            advancedStorage.methods
-                .add(data)
-                .send({from: accounts[0]})
-                .then (() => {
-                    return advancedStorage.methods.getAll()
+    .then(_accounts => {
+        accounts = _accounts;
+        return advancedStorage.methods
+            .getAll()
+            .call();
+    })
+    .then(result => {
+        $data.innerHTML = result.join(', ');
+    });
+
+    $addData.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const data = e.target.elements[0].value;
+        advancedStorage.methods
+            .add(data)
+            .send({from: accounts[0]})
+            .then (result => {
+                return advancedStorage.methods
+                    .getAll()
                     .call();
-                })
-                .then(result => {
-                    $data.innerHTML = result.join(', ');
-                });
-        });
+            })
+            .then(result => {
+                $data.innerHTML = result.join(', ');
+            });
+    });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
