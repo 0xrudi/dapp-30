302a7586877197eaa4901754e997cd5c6831dc01
rudnickandrew <andrewstevenrudnick@gmail.com>
2023-02-28 09:26:56 -0800

    day 3 project start

--- day-3-simple-storage/test/simpleStorage.js
+++ day-3-simple-storage/test/simpleStorage.js
@@ -1,6 +1,7 @@
 const SimpleStorage = artifacts.require('SimpleStorage');
 
 contract('SimpleStorage', () => {
+  // unit test to validate the set function will update the data variable within the smart contract
   it('should set the value of data variable in smart contract', async () => {
     const simpleStorage = await SimpleStorage.deployed();
     await simpleStorage.set('this');
