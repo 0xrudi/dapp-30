2a5903cf1fdae212285f2b3ecbadcc77e6a0c9b1
rudnickandrew <andrewstevenrudnick@gmail.com>
2023-03-01 09:56:26 -0800

    Finish refactoring smart contract

--- day-5-crud/contracts/Crud.sol
+++ day-5-crud/contracts/Crud.sol
@@ -7,7 +7,7 @@ contract Crud {
     }
 
     User[] public users;
-    uint public nextId;
+    uint public nextId = 1;
 
     function create(string memory name) public {
         // this function will input the next id available and the entered name and push to the users array
@@ -18,32 +18,33 @@ contract Crud {
 
     function read(uint id) view public returns(uint, string memory) {
         // uses the helper function to locate the record to update
-        i = find(id); 
+        uint i = find(id); 
         return (users[i].id, users[i].name);
     }
 
     function update(uint id, string memory name) public {
         // uses the helper function to locate the record to update
-        i = find(id);
+        uint i = find(id);
         // uses dot function to update the name for the specific record
-        return(users[i].name = name);
-        }
+        users[i].name = name;
     }
 
     function destroy(uint id) public {
         // uses the helper function to locate the record to update
-        i = find(id);
+        uint i = find(id);
         delete users[i];
     }
 
     // below is a helper function to remove redundant code from public functions
     // this helper function will be internal since it only should be called by the smart contract
     // using a for loop to iterate through the users array
-    function find(uint id) view internal returns {uint} {
+    function find(uint id) view internal returns (uint) {
         for (uint i = 0; i < users.length; i++) {
             if(users[i].id == id) {
                 return i;
             }
         }
+        revert('user does not exist!');
     }
 }
+
