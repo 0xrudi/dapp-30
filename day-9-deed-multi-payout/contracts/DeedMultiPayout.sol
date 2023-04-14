9f56ad45e0d8422fc500d1ed5ff3ae3f3bdad7a6
rudnickandrew <andrewstevenrudnick@gmail.com>
2023-04-14 11:23:30 -0700

    bug fixes to smart contract code

--- day-9-deed-multi-payout/contracts/DeedMultiPayout.sol
+++ day-9-deed-multi-payout/contracts/DeedMultiPayout.sol
@@ -14,8 +14,9 @@ contract DeedMultiPayout {
     constructor (
         address _lawyer,
         address payable _beneficiary, 
-        uint fromNow
-        ) payable public {
+        uint fromNow) 
+        payable 
+        public {
             lawyer = _lawyer;
             beneficiary = _beneficiary;
             earliest = now + fromNow;
@@ -23,23 +24,19 @@ contract DeedMultiPayout {
         }
 
     function withdraw() public {
-        require(msg.sender == beneficiary, 'only beneficiary can withdrawal');
+        require(msg.sender == lawyer, 'only lawyer can withdrawal');
         require(now >= earliest, 'amount can not yet be distributed');
         require(paidPayouts < PAYOUTS, 'no payouts left');
-        // we have to introduce a new variable to prevent a reentrancy vulnerability
-        // here lastWithdrawal will store the date of when the last withdrawal took place
-        // instead of the below logic, we are going to perform the check using duePayouts below
-        // uint lastWithdrawal;
 
         // elligible payouts will calculate the amount of payments that have been earned over the elapsed time period since
-        uint elligiblePayouts = (now - earliest) / INTERVAL;
+        uint elligiblePayouts = 1 + (now - earliest) / INTERVAL;
 
         // instead of the logic above, we are going to leverage the paidPayouts variable
         // this will allow us to keep track of how many payouts have taken place explicitly
         uint duePayouts = elligiblePayouts - paidPayouts;
-        paidPayouts += duePayouts;
 
-        duePayouts = duePayouts + paidPayouts >= PAYOUTS ? PAYOUTS - paidPayouts : duePayouts;
+        duePayouts = duePayouts + paidPayouts > PAYOUTS ? PAYOUTS - paidPayouts : duePayouts;
+        paidPayouts += duePayouts;
         // instead of transfering the full amount of the smart contract balance, we want to send the broken up amount multiplied
         // by the amount of elligible payments
         beneficiary.transfer(duePayouts * amount);
--- day-9-deed-multi-payout/test/deedMultiPayouts.js
+++ day-9-deed-multi-payout/test/deedMultiPayouts.js
@@ -10,8 +10,9 @@ contract('DeedMultiPayout', (accounts) => {
         for(let i = 0; i < 4; i++) {
             const initialBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
             await new Promise(resolve => setTimeout(resolve, 1000));
-            await deedMultiPayout.withdraw({from: accounts[1]});
+            await deedMultiPayout.withdraw({from: accounts[0]});
             const finalBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+            console.log(finalBalance.sub(initialBalance).toNumber());
             assert(finalBalance.sub(initialBalance).toNumber() === 25);
         }
     });
@@ -19,17 +20,18 @@ contract('DeedMultiPayout', (accounts) => {
     it('should withdraw for all payouts (2)', async () => {
         const deedMultiPayout = await DeedMultiPayout.new(accounts[0], accounts[1], 1, {value: 100});
         for(let i = 0; i < 2; i++) {
-            const balanceBefore = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+            const initialBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
             await new Promise(resolve => setTimeout(resolve, 2000));
-            await deedMultiPayout.withdraw({from: accounts[1]});
-            const balanceAfter = await web3.utils.toBN(web3.eth.getBalance(accounts[1]));
-            assert(balanceAfter.sub(balanceBefore).toNumber() === 50);
+            await deedMultiPayout.withdraw({from: accounts[0]});
+            const finalBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+            console.log(finalBalance.sub(initialBalance).toNumber());
+            assert(finalBalance.sub(initialBalance).toNumber() === 50);
         }
     });
 
     it('should NOT withdraw if no payout left', async () => {
         try {
-          await deedMultiPayout.withdraw({from: accounts[1]});
+          await deedMultiPayout.withdraw({from: accounts[0]});
         } catch (e) {
           assert(e.message.includes('no payouts left'));
           return;
@@ -40,7 +42,7 @@ contract('DeedMultiPayout', (accounts) => {
     it('should not withdraw if too early', async () => {
         const deedMultiPayout = await DeedMultiPayout.new(accounts[0], accounts[1], 5, {value: 100});
         try {
-            await deedMultiPayout.withdraw({from: accounts[1]});
+            await deedMultiPayout.withdraw({from: accounts[0]});
         } catch (e) {
             assert(e.message.includes('amount can not yet be distributed'));
             return;
@@ -51,10 +53,9 @@ contract('DeedMultiPayout', (accounts) => {
     it('should not withdraw if sender is not lawyer', async () => {
         const deedMultiPayout = await DeedMultiPayout.new(accounts[0], accounts[1], 5, {value: 100});
         try {
-            await new Promise(resolve => setTimeout(resolve, 5000));
             await deedMultiPayout.withdraw({from: accounts[4]});
         } catch (e) {
-            assert(e.message.includes('only beneficiary can withdrawal'));
+            assert(e.message.includes('only lawyer can withdrawal'));
             return;
         }
         assert(false);
